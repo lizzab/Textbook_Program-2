b60622a01060f3417000252bfeef847f5dd0e4c6
lizzab <Jjibber12>
2019-11-22 11:57:37 -0500

    Update

---
+++ Person_module.py
@@ -0,0 +1,12 @@
+# Ben Lizza
+# 11/20/19
+# Module that will include a Person class and a Textbook that will use Person class in author
+
+
+class Person:
+    def __init__(self, first_name, last_name, age):
+        self.first = first_name
+        self.last = last_name
+        self.age = age
+
+
